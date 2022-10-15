02d526d11452646b93fe7593439f31a0683e2c24
unknown <wee.gee60@yahoo.com>
2022-10-15 23:36:05 +0200

    Revert "Update sound.c"
    This reverts commit ea932cf4297ca01f25d76d42db7e57cabcfb16b7.

--- src/sound.c
+++ src/sound.c
@@ -136,13 +136,10 @@ enum{
 	SAMPLE_CLICK,
 	NUM_SAMPLES
 };
-
-char tmpchar[256];
-snprintf(tmpchar, sizeof(tmpchar), "%s%s%s", progpath, DATA_PREFIX "click.wav");
-printf("tmpchar 1: %s\n", tmpchar);
-	
 static char *sample_filename[NUM_SAMPLES]={
-	tmpchar	
+	char tmpchar[256];
+	snprintf(tmpchar, sizeof(tmpchar), "%s%s%s", progpath, DATA_PREFIX "click.wav");
+	printf("tmpchar 1: %s\n", tmpchar);
 };
 static Mix_Chunk *sample_wave[NUM_SAMPLES];
 #define play_sample(NSAMPLE) Mix_PlayChannel(0,sample_wave[(NSAMPLE)],0)
