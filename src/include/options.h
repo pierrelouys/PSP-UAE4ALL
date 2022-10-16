e7b551e3332737bc71874b957383cc4739c74b14
unknown <wee.gee60@yahoo.com>
2022-10-16 03:25:03 +0200

    Update options.h

--- src/include/options.h
+++ src/include/options.h
@@ -26,8 +26,8 @@ extern unsigned prefs_chipmem_size;
 extern int prefs_gfx_framerate, changed_gfx_framerate;
 extern int m68k_speed;
 
-#define PREFS_GFX_WIDTH 320
-#define PREFS_GFX_HEIGHT 200
+#define PREFS_GFX_WIDTH 480
+#define PREFS_GFX_HEIGHT 270
 
 extern void check_prefs_changed_custom (void);
 extern void check_prefs_changed_cpu (void);
