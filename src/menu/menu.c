5770c9bcc5f814597ab60f0a7d24dbd47b58dc11
unknown <wee.gee60@yahoo.com>
2022-10-16 02:00:09 +0200

    Update menu.c

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -197,7 +197,7 @@ void init_text(int splash)
 	if (prSDLScreen==NULL)
 	{
 		SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_JOYSTICK);
-		prSDLScreen=SDL_SetVideoMode(480,272,16,VIDEO_FLAGS);
+		prSDLScreen=SDL_SetVideoMode(320,272,16,VIDEO_FLAGS);
     		SDL_ShowCursor(SDL_DISABLE);
  	   	SDL_JoystickEventState(SDL_ENABLE);
     		SDL_JoystickOpen(0);
